8d0cc090de0443d2f544a7fca783639ffd98fb28
Hernan Yarbi <hernan.yarbi@gmail.com>
2017-11-27 19:41:41 -0300

    node

---
+++ api/app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let express = require('express');
+let bodyParser = require('body-parser');
+let app = express();
+// var productoRoutes = require('./routes/productos');
+// var clienteRoutes = require('./routes/cliente');
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+app.use((req, res ,next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT ,DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT ,DELETE');
+
+  next();
+});
+// app.use('/api', productoRoutes);
+// app.use('/api', clienteRoutes);
+
+module.exports = app;
